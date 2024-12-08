01e3d74c7bef6b4309d5ba9524d3da2ec9b7e2a5
LEAAKC <laboratorioensayosdeaguas@gmail.com>
2024-12-08 11:26:09 -0500

    enviando email ok, apppassword working

--- LEA_SERVER/src/utils/mailer.js
+++ LEA_SERVER/src/utils/mailer.js
@@ -11,8 +11,8 @@ console.log('Email User:', process.env.NODE_EMAIL_USER);
 console.log('Email Pass:', process.env.NODE_EMAIL_PASS);
 
 
-cron.schedule('* * * * *', async () => {  // Ejecutar cada minuto
-  console.log("Ejecutando node-cron cada minuto");
+cron.schedule('35 11 * * *', async () => {  // Ejecutar cada minuto
+  console.log("Ejecutando node-cron a las 11:35");
 
   try {
     // Obtener productos próximos a vencer
@@ -23,7 +23,7 @@ cron.schedule('* * * * *', async () => {  // Ejecutar cada minuto
     productosProximos.forEach((producto) => {
       const body = `El producto ${producto.nombre} con lote ${producto.lote} está a punto de vencer el ${producto.fechaVencimiento}.`;
       console.log(body);
-      //sendEmailData('Aviso de vencimiento', body);
+      sendEmailData('Aviso proximo a vencer', body);
     });
 
     // Obtener productos vencidos
@@ -34,7 +34,7 @@ cron.schedule('* * * * *', async () => {  // Ejecutar cada minuto
     productosVencidos.forEach((producto) => {
       const body = `El producto ${producto.nombre} con lote ${producto.lote} ha vencido el ${producto.fechaVencimiento}.`;
       console.log(body);  // Agrega un log aquí para verificar
-      //sendEmailData('Aviso de vencimiento', body);
+      sendEmailData('Aviso de vencimiento', body);
     });
   } catch (error) {
     console.error('Error al ejecutar las funciones:', error);
@@ -45,23 +45,26 @@ const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: process.env.NODE_EMAIL_USER,  // Tu dirección de correo
-    pass: process.env.NODE_EMAIL_PASS,  // Contraseña de la cuenta o contraseña de aplicación
+    pass: process.env.app_password_gmail,  // Contraseña de la cuenta o contraseña de aplicación
   },
   tls: {
-    rejectUnauthorized: false, // Esto permite conexiones con certificados autofirmados, si es necesario
-  },
-  // Puedes cambiar el puerto si es necesario:
-  // port: 587,
-  // secure: false, // Si usas STARTTLS
+    rejectUnauthorized: false
+  }
 });
 
 export const sendEmailData = async (subject, body) => {
+  console.log("subject en sendEmailData:", subject);
+  console.log("body en sendEmailData:", body);
+  
   const mailOptions = {
     from: process.env.NODE_EMAIL_USER,
     to: process.env.NODE_TO,
     subject,
     text: body,
   };
+
+console.log("mailOptions:", mailOptions);
+
 
   try {
     const info = await transporter.sendMail(mailOptions);
